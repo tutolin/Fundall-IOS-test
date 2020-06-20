848bafd315f2d6d6af3599e37df8f1eca6f43f55
tutolin <aboyejitolu@gmail.com>
2020-06-20 13:49:49 +0100

    Initial Commit

---
+++ Fundall UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fundall UI
+//
+//  Created by Aboyeji Tolulope on 20/06/2020.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
